6b21942be439d6b8669afd1220d40ea36be6531e
Jorge Luid Sarmiento Herrera <yorchsarmiento@gmail.com>
2013-09-03 19:32:37 -0500

    Lesson 3 Adding Action Buttons to Action Bar (No icons)

--- src/com/globant/mobile/handson/MainActivity.java
+++ src/com/globant/mobile/handson/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -22,6 +23,24 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		//Handle presses on the action bar items
+		switch(item.getItemId()){
+			case R.id.action_settings:{
+				//openSettings();
+				return true;
+			}
+			case R.id.action_search:{
+				//openSearch();
+				return true;
+			}
+			default:{
+				return super.onOptionsItemSelected(item);
+			}
+		}
 	}
 	
 	public void sendMessage(View view){
